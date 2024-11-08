fcfeac0da3d480bc6d7a301d0fa6fc7b4ac9f357
Madison Manning <mmannin3@uwyo.edu>
2024-11-08 20:12:01 +0000

    omw

--- Manning-COSC1010-lab08.py
+++ Manning-COSC1010-lab08.py
@@ -82,6 +82,7 @@ while True:
     x1 = check_numbers(x1)
     x2 = input("X upper bound")
     x2 = check_numbers(x2)
+
     if m != False and b != False and x1 != False and x2 != False:
         if type(x1) == float or type(x2) == float:
             print(f"please enter whole X numbers")
@@ -102,3 +103,28 @@ print("*" * 75)
 # Create a loop like above to prompt the user for input for the three values
 # Create a second function that just does the square root operation 
     # If the number you are trying to take the square root of is negative, return null
+
+def sq_root(a, b, c):
+    inner_eq = (b ** 2) - (4 * a * c)
+    numbers = []
+    added_num = 0
+    sub_num = 0
+    if inner_eq < 0:
+        return null
+    else:
+        numbers.append(((-1 * b) + (inner_eq ** 0.5)) / (2 * a))
+        numbers.append(((-1 * b) - (inner_eq ** 0.5)) / (2 * a))
+        return numbers
+
+while True:
+    a = input("a, exit to quit")
+    if a.lower() == "exit":
+        break
+    a = check_numbers(a)
+    b = input("b")
+    b = check_numbers(b)
+    c =  input("c")
+    c = check_numbers(c)
+
+    if a != False and b != False and c != False and d != False:
+        
